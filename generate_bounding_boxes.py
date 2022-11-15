f3eb4b454e86cecf2465fa36150381acbf15f1d3
zsiciliani <59708168+zsiciliani@users.noreply.github.com>
2022-11-14 21:38:58 -0500

    Change script to work on all files; rename script

---
+++ generate_bounding_boxes.py
@@ -0,0 +1,102 @@
+import math
+import numpy as np
+import json
+import sys
+import os
+
+if __name__ == "__main__":
+    PATH_TO_VIDEOPOSE = input("Enter the absolute path to your video_pose directory:")
+    VIDEO_POSE_TYPES = {"No_penalty": 0, "Slashing": 1, "Tripping": 2}
+
+    annotation_id = 0  # increment this to have unique id for each annotation
+    for video_dir_name in os.listdir(PATH_TO_VIDEOPOSE):
+        video_dir_full_path = os.path.join(PATH_TO_VIDEOPOSE, video_dir_name)
+
+        if (
+            not os.path.isfile(video_dir_full_path)
+            and video_dir_name in VIDEO_POSE_TYPES.keys()
+        ):
+
+            print(f"Adding bbox attribute to coco jsons in {video_dir_name}:")
+            for game_dir_name in os.listdir(video_dir_full_path):
+                game_dir_full_path = os.path.join(video_dir_full_path, game_dir_name)
+                if not os.path.isfile(game_dir_full_path):
+                    print(f"Converting {game_dir_full_path}...")
+
+                    f = open(f"{game_dir_full_path}/{game_dir_name}-coco.json")
+                    data = json.load(f)
+                    f.close()
+
+                    #get image width and height, assuming they are the same size for all images in one video
+                    imageWidth = data["images"][0]["width"]
+                    imageHeight = data["images"][0]["height"]
+
+                    for player in data["annotations"] :
+                        playerNumberString = "p" + str(player["category_id"])
+                        playerKeypoints = player["keypoints"]
+                        minx = 99999
+                        maxx = -99999
+                        miny = 99999
+                        maxy = -99999
+
+                        # Currently ignoring the stick; should it be included in the box?
+                        for i in range(14) :
+                            #if out of frame, skip this keypoint
+                            if playerKeypoints[i * 3] == 0 and playerKeypoints[i * 3 + 1] == 0 :
+                                continue
+
+                            #otherwise, check for a new minx, miny, maxx, maxy
+                            if playerKeypoints[i * 3] < minx :
+                                minx = playerKeypoints[i * 3]
+                            if playerKeypoints[i * 3] > maxx : 
+                                maxx = playerKeypoints[i * 3]
+                            if playerKeypoints[i * 3 + 1] < miny :
+                                miny = playerKeypoints[i * 3 + 1]
+                            if playerKeypoints[i * 3 + 1] > maxy : 
+                                maxy = playerKeypoints[i * 3 + 1]
+
+                        bbox = [0,0,0,0]
+
+                        if minx == 99999 or maxx == -99999 or miny == 99999 or maxy == -99999 :
+                            #player is fully out of frame, set bounding box to 0,0,0,0
+                            bbox = [0,0,0,0]
+                        else :
+                            # Box formed just around the keypoints will be too small; need to grow it
+                            # Easy proposal: grow it on all sides by the half-face distance (distance from nose to neck)
+                            # This may need to be tinkered with to get more accurate boxes
+                            # Also: what to do when either is out of frame?
+                            nosex = playerKeypoints[0]
+                            nosey = playerKeypoints[1]
+                            neckx = playerKeypoints[3]
+                            necky = playerKeypoints[4]
+
+                            halfFaceDistance = 0
+                            # don't calculate half face distance if nose or neck is out of frame
+                            if (not ((nosex == 0 and nosey == 0) or (neckx == 0 and necky == 0))):
+                                halfFaceDistance = math.sqrt((nosex - neckx) * (nosex - neckx) + (nosey - necky) * (nosey - necky))
+
+                            # Increment the size of the bounding box, making sure to stay in frame
+                            minx = np.maximum(0.0, minx - halfFaceDistance)
+                            maxx = np.minimum(float(imageWidth), maxx + halfFaceDistance)
+                            miny = np.maximum(0.0, miny - halfFaceDistance)
+                            maxy = np.minimum(float(imageHeight), maxy + halfFaceDistance)
+
+                            width = maxx - minx
+                            height = maxy - miny
+
+                            #bbox is format x,y,width,height, where x,y is the top left corner of the box
+                            bbox = [minx, miny, width, height]
+                        
+                        # Set the bbox attribute in the json data
+                        player["bbox"] = bbox
+                    
+                    # Write the json to a file and save it
+                    outputFilenameString = f"{game_dir_full_path}/{game_dir_name}-bbox-appended.json"
+                    outputFile = open(outputFilenameString, "w")
+                    outputJson = json.dump(data, outputFile, indent=4)
+                    outputFile.close()
+
+
+
+
+
