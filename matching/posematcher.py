29c84bf306c24145817308803122b65ff8745ec5
Viet Tran <viet.tran.gm@gmail.com>
2023-01-23 18:10:32 -0500

    Change pose matching

---
+++ matching/posematcher.py
@@ -0,0 +1,93 @@
+from collections import defaultdict
+import json
+import pickle
+import numpy as np
+import os
+from matching.norm_pose import procrustes
+from tqdm import tqdm
+
+
+class PoseMatcher():
+
+    def __init__(self, top_down_path, btm_up_path):
+        self.top_down_path = top_down_path
+        self.btm_up_path = btm_up_path
+
+    def _best_match(self, ref, targets):
+
+        def OKS(p1, p2):
+            sigma = 0.05
+            # selected_idx = np.int64([14, 8,9, 11,12, 5,6, 2,3])
+            p1_selected = p1[:2]
+            p2_selected = p2[:2]
+            # p1_selected -= p1_selected[:,0:1]
+            # p2_selected -= p2_selected[:,0:1]
+            dist = np.square(p1_selected - p2_selected).sum(axis=0)
+            result = np.exp(-dist / (2 * sigma**2)).mean()
+            return result
+
+        max_idx = 0
+        max_oks = 0
+        max_pts = 0
+        for i in range(len(targets)):
+            aligned_target = procrustes(targets[i], ref)
+            oks = OKS(ref, aligned_target)
+            if oks > max_oks:
+                max_oks = oks
+                max_idx = i
+                max_pts = aligned_target
+        return max_pts, max_idx
+
+    def _format_keypoints(self, keypoints):
+        x_coord = []
+        y_coord = []
+        confidence = []
+        split = np.split(np.array(keypoints), len(keypoints) // 3)
+
+        for x, y, c in split:
+            x_coord.append(x)
+            y_coord.append(y)
+            confidence.append(c)
+
+        return np.array(
+            [np.array(x_coord),
+             np.array(y_coord),
+             np.array(confidence)])
+
+    def match(self, pts_out_path):
+        # create directory
+        if not os.path.exists(pts_out_path):
+            os.makedirs(pts_out_path)
+
+        # Load predictions
+        bu_estimations = {}
+        bu_predictions = json.load(open(self.btm_up_path))
+        for pred in bu_predictions:
+            image_id = pred["image_id"]
+            if image_id in bu_estimations:
+                bu_estimations[image_id].append(pred)
+            else:
+                bu_estimations[image_id] = [pred]
+
+        # we sort here so it's in correct order, to prevent some linux os produce incorrect order
+        td_estimations = json.load(open(self.top_down_path))
+        prev = None
+        results = []
+
+        for t in tqdm(td_estimations):
+            img_id = t['image_id']
+
+            td_pts = self._format_keypoints(t["keypoints"])
+            # match
+            bu_pred = bu_estimations[img_id]
+            bu_pred = np.array([self._format_keypoints(pred["keypoints"]) for pred in bu_pred])
+            p_aligned, _ = self._best_match(td_pts, bu_pred)
+            
+            if not (prev and prev == img_id):
+                pickle.dump(
+                    results,
+                    open(os.path.join(pts_out_path, '%d.pkl' % img_id), 'wb'))
+                results.clear()
+            results.append(p_aligned)
+                
+            prev = img_id
